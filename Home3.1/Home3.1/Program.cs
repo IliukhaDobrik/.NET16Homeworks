0ba4ab2e4dfcf718d1c52e7dafe4bfb4294038d0
IliykhaDobrinskiy <bogdobrinskiy@gmail.com>
2022-07-16 16:12:10 +0300

    ADDED example from third homework

---
+++ Home3.1/Home3.1/Program.cs
@@ -0,0 +1,35 @@
+﻿Console.Write("Enter number A: ");
+int num1 = int.Parse(Console.ReadLine());
+
+Console.Write("Enter number B: ");
+int num2 = int.Parse(Console.ReadLine());
+
+Console.Write("Enter number C: ");
+int num3 = int.Parse(Console.ReadLine());
+
+if (num1 > 100 && num2 > 100)
+{
+    Console.WriteLine("A > 100 && B < 100");
+}
+if (num1 % 2 == 0 || num2 % 2 == 0)
+{
+    Console.WriteLine("A % 2 == 0 || B % 2 == 0");
+}
+if (num1 > 0 || num2 > 0)
+{
+    Console.WriteLine("A > 0 || B > 0");
+}
+if (num1 % 3 == 0 && num2 % 3 == 0 && num3 % 3 == 0)
+{
+    Console.WriteLine("A % 3 == 0 && B % 3 == 0 && C % 3 == 0");
+}
+if (num1 < 50 || num2 < 50 || num3 < 50)
+{
+    Console.WriteLine("A < 50 || B < 50 || C < 50");
+}
+if (num1 < 0 || num2 < 0 || num3 < 0)
+{
+    Console.WriteLine("A < 0 || B < 0 || C < 0");
+}
+
+Console.ReadKey();
--- README.md
+++ README.md
@@ -1,4 +1,5 @@
-This repository is for homeworks
-ADDED first homework
-ADDED second homework
-ADDED third homework
+ThIs Is RePoSiToRy FoR hOmEwOrKs;
+ADDED first homework(Home1);
+ADDED second homework(Home2);
+ADDED third homework(Home3);
+ADDED example from third homework(Home3.1);
