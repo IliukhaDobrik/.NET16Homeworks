c3d14751a93ca3129d0f99af92009803f34a09a5
IliykhaDobrinskiy <bogdobrinskiy@gmail.com>
2022-08-07 10:39:24 +0300

    CREATED class Card

---
+++ Home9Taxi/Home9Taxi/Card.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Home9Taxi
+{
+    internal class Card : IPaymentMethod
+    {
+        public Card(string cardNumber, double amountOfMoney)
+        {
+            CardNumber = cardNumber;
+            AmountOfMoney = amountOfMoney;
+        }
+
+        public string CardNumber { get; }
+
+        public double AmountOfMoney { get; private set; }
+
+        public void AddMoney(double money)
+        {
+            AmountOfMoney += money;
+        }
+
+        public bool IsPaymentPossible(double cost)
+        {
+            if (cost > AmountOfMoney)
+            {
+                return false;
+            }
+
+            return true;
+        }
+
+        public void MakePayment(double cost)
+        {
+            if (IsPaymentPossible(cost))
+            {
+                AmountOfMoney -= cost;
+            }
+            else
+            {
+                throw new Exception("Недостаточно денег");
+            }
+        }
+    }
+}
