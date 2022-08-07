68817615f1043cf31e657a41d3be496a2faf99ae
IliykhaDobrinskiy <bogdobrinskiy@gmail.com>
2022-08-07 11:02:34 +0300

    CREATED class Point and some FIXED

---
+++ Home9Taxi/Home9Taxi/Point.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Home9Taxi
+{
+    internal class Point : IPaymentMethod
+    {
+        public double AmountOfPoints { get; private set; } = 0;
+        public double AmountOfMoney { get; private set; } = 0;
+
+        public void AddPoints(double points)
+        {
+            AmountOfPoints += points;
+            AmountOfMoney += AmountOfPoints * 3;
+        }
+
+        public void AddMoney(double money)
+        {
+            AmountOfMoney += money;
+            AmountOfPoints += AmountOfMoney / 3;
+        }
+
+        public bool IsPaymentPossible(double points)
+        {
+            if (AmountOfPoints < points)
+            {
+                return false;
+            }
+            else
+            {
+                return true;
+            }
+        }
+
+        public void MakePayment(double points)
+        {
+            if (IsPaymentPossible(points))
+            {
+                AmountOfPoints -= points;
+                AmountOfMoney -= points * 3;
+            }
+            else
+            {
+                throw new Exception("Недостаточно бонусных баллов!");
+            }
+        }
+    }
+}
