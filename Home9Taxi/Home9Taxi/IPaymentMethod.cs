7c1ebfa89a4288234eda17df3d02afc628d94ff1
IliykhaDobrinskiy <bogdobrinskiy@gmail.com>
2022-08-07 10:04:07 +0300

    CREATED IPaymentMethod

---
+++ Home9Taxi/Home9Taxi/IPaymentMethod.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Home9Taxi
+{
+    internal interface IPaymentMethod
+    {
+        bool IsPaymentPossible(double money);
+        void MakePayment(double money);
+        void AddMoney(double money);
+    }
+}
---
+++ Home9Taxi/Home9Taxi/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace Home9Taxi
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
