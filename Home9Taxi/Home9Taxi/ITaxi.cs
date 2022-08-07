1640670d1bc98c710f0d8b1b6cc447a278bb4530
IliykhaDobrinskiy <bogdobrinskiy@gmail.com>
2022-08-07 19:06:52 +0300

    CREATED interface ITaxi

---
+++ Home9Taxi/Home9Taxi/ITaxi.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Home9Taxi
+{
+    internal interface ITaxi
+    {
+        void MakeRide(in User user);
+        double GetPriceOfRide();
+    }
+}
