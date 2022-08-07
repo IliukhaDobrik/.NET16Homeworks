7fa2f19b71503a4cde32efd9201286bd3e43f7f2
IliykhaDobrinskiy <bogdobrinskiy@gmail.com>
2022-08-07 19:14:59 +0300

    CREATED class Vehicle

---
+++ Home9Taxi/Home9Taxi/Vehicle.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Home9Taxi
+{
+    internal class Vehicle
+    {
+        private double _fuelConsumption = 0;
+
+        public Vehicle() { }
+
+        public Vehicle(string govermentNumber, double fuelConsumption)
+        {
+            GovermentNumber = govermentNumber;
+            FuelConsumption = fuelConsumption;
+        }
+
+        public string GovermentNumber { get; init; } = string.Empty;
+        public double FuelConsumption
+        {
+            get => _fuelConsumption;
+            set
+            {
+                if (value < 0)
+                {
+                    _fuelConsumption = 0;
+                }
+                else
+                {
+                    _fuelConsumption = value;
+                }
+            }
+        }
+    }
+}
