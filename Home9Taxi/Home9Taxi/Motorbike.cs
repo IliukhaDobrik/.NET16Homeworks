55cdaa3e6576c06c2feb5716b025aba79c163c27
IliykhaDobrinskiy <bogdobrinskiy@gmail.com>
2022-08-07 19:36:46 +0300

    CREATED class Motorbike

---
+++ Home9Taxi/Home9Taxi/Motorbike.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Home9Taxi
+{
+    internal class Motorbike : Vehicle, ITaxi
+    {
+        public static int MaxSpeed = 200;
+        public bool IsWithSidecar { get; set; }//с коляской ли мотоцикл
+        public int Power { get; init; } = 0;
+
+        public double GetPriceOfRide()
+        {
+            return 10;
+        }
+
+        public void MakeRide(in User user)
+        {
+            if (IsWithSidecar)
+            {
+                Console.WriteLine($"{user.FirstName} {user.LastName} совершил(ла) поездку на " +
+                    $"мотоцикле, мощность {Power}, с коляской");
+            }
+            else
+            {
+                Console.WriteLine($"{user.FirstName} {user.LastName} совершил(ла) поездку на " +
+                    $"мотоцикле, мощность {Power}, без коляски");
+            }
+        }
+    }
+}
