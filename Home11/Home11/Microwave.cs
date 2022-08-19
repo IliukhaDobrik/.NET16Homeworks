47872ec6d5a1500059bef0bf3f905815b3efd04a
IliykhaDobrinskiy <bogdobrinskiy@gmail.com>
2022-08-19 10:38:33 +0300

    ADDED Home11

---
+++ Home11/Home11/Microwave.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Home11
+{
+    delegate void NotifyWarmUpCompleted(string nameOfDish);
+    internal class Microwave
+    {
+        public event NotifyWarmUpCompleted? WarmUpCompleted;
+
+        public void WarmUp(string nameOfDish)
+        {
+            Console.WriteLine($"Я подогреваю {nameOfDish}");
+            WarmUpCompleted?.Invoke(nameOfDish);
+        }
+    }
+}
---
+++ Home11/Home11/Program.cs
@@ -0,0 +1,19 @@
+﻿namespace Home11
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Microwave microwave = new Microwave();
+            microwave.WarmUpCompleted += WarmUpCompletedHandler;
+
+            microwave.WarmUp("Драники");
+            microwave.WarmUp("Шавуха");
+        }
+
+        static void WarmUpCompletedHandler(string nameOfDish)
+        {
+            Console.WriteLine($"Ваша еда готова: {nameOfDish}");
+        }
+    }
+}
