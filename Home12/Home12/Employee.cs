a669f5b8554950831dd0181dcece819c8cfafd8b
IliykhaDobrinskiy <bogdobrinskiy@gmail.com>
2022-08-23 11:38:31 +0300

    ADDED Home12

---
+++ Home12/Home12/Employee.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Home12
+{
+    internal class Employee
+    {
+        private int _age = 0;
+        private string _name = string.Empty;
+        private double _salary = 0d;
+
+        public int Age
+        {
+            get => _age;
+            set
+            {
+                if (value < 18)
+                {
+                    throw new ArgumentException("Age less then 18!");
+                }
+                _age = value;
+            }
+        }
+
+        public string Name
+        {
+            get => _name;
+            set
+            {
+                if (string.IsNullOrEmpty(value))
+                {
+                    throw new ArgumentNullException("Name is null or empty!");
+                }
+                if (value.Length >= 100)
+                {
+                    throw new ArgumentException("Your name is so long1");
+                }
+                _name = value;
+            }
+        }
+
+        public double Salary
+        {
+            get => _salary;
+            set
+            {
+                if (value < 0)
+                {
+                    throw new ArgumentException("Salary can't be negative!");
+                }
+                _salary = value;
+            }
+        }
+    }
+}
---
+++ Home12/Home12/Program.cs
@@ -0,0 +1,32 @@
+﻿using System.Reflection;
+
+namespace Home12
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var employee = new Employee();
+
+            try
+            {
+                employee.Name = "Ilya";
+                employee.Age = 20;
+                employee.Salary = 0;
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+                Console.WriteLine(ex.StackTrace);
+            }
+
+            Type type = employee.GetType();
+
+            foreach (PropertyInfo propertyInfo in type.GetProperties())
+            {
+                var prop = type.GetProperty(propertyInfo.Name);
+                Console.WriteLine($"{propertyInfo.Name} -- {prop?.GetValue(employee)}");
+            }
+        }
+    }
+}
