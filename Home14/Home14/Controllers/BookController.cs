6d9a932cef3d62ec1a71e0974fc602dfa0121604
IliukhaDobrik <bogdobrinskiy@gmail.com>
2022-09-01 19:23:55 +0300

    ADDED Home14

---
+++ Home14/Home14/Controllers/BookController.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Home14.Models;
+
+namespace Home14.Controllers
+{
+    public class BookController : Controller
+    {
+        public IActionResult Index()
+        {
+            var book = new BookViewModel { Title = "La Divina Commedia", Author = "Dante Alighieri", CountOfPages = 798};
+
+            return View(book);
+        }
+    }
+}
---
+++ Home14/Home14/Models/BookViewModel.cs
@@ -0,0 +1,9 @@
+﻿namespace Home14.Models
+{
+    public class BookViewModel
+    {
+        public string Title { get; set; } = string.Empty;
+        public string Author { get; set; } = string.Empty;
+        public int CountOfPages { get; set; } = 0;
+    }
+}
