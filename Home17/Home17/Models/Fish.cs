31292aed4881c7ce61455efc2a4532d0710ef9e7
IliukhaDobrik <bogdobrinskiy@gmail.com>
2022-09-08 20:04:56 +0300

    ADDED in folder Models class Fish.cs

---
+++ Home17/Home17/Models/Fish.cs
@@ -0,0 +1,9 @@
+﻿namespace Home17.Models
+{
+    public class Fish
+    {
+        public string Name { get; set; } = string.Empty;
+        public double Weight { get; set; } = 0d;
+        public double Speed { get; set; } = 0d;
+    }
+}
